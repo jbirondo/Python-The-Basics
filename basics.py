95fe5c7a59b50c95a75bb6de7e8a6033569864c5
Eugene Birondo <jbirondo@gmail.com>
2020-06-16 13:06:35 -0700

    How to find out what code you need

--- basics.py
+++ basics.py
@@ -19,4 +19,8 @@ import datetime
 # print(sum1, sum2)
 # print(type(x), type(y), type(z))
 
-student_grades = [9.1, 8,8, 7.5]
+student_grades = [9.1, 8.8, 7.5]
+mysum = sum(student_grades)
+length = len(student_grades)
+mean = mysum / length
+print(mean)
