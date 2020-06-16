67f7ad1b3f5b3b6a30c13be56109807b4ab61728
Eugene Birondo <jbirondo@gmail.com>
2020-06-16 16:34:49 -0700

    Accessing itesms and slices with negative indeces

--- basics.py
+++ basics.py
@@ -36,3 +36,5 @@ monday_temperatures = [9.1, 8.8, 7.5]
 # monday_temperatures.index(8.8) returns the first index of the value searched
 # monday_temperatures.__getitem__(1) or monday_temperatures[1] get item at index
 # monday_temperatures[1:2] or monday_temperature[:2] (from beginning) or monday_temperature[2:] (from index to the end) slice of list where first argument is the beginning and the last agrument is the last index not inclusive 
+# monday_temperatures[-1] access the last index
+# monday_temperatures[-2:] slice of the last two items of the list
