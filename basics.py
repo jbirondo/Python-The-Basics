8ebdc9de031a6da3903d0f065c5c6fdcdabf64eb
Eugene Birondo <jbirondo@gmail.com>
2020-06-16 16:36:18 -0700

    String slicing

--- basics.py
+++ basics.py
@@ -38,3 +38,4 @@ monday_temperatures = [9.1, 8.8, 7.5]
 # monday_temperatures[1:2] or monday_temperature[:2] (from beginning) or monday_temperature[2:] (from index to the end) slice of list where first argument is the beginning and the last agrument is the last index not inclusive 
 # monday_temperatures[-1] access the last index
 # monday_temperatures[-2:] slice of the last two items of the list
+# slicing works on strings as well
