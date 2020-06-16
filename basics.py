3bff9812db89d6b7827d635a9337322341f70daa
Eugene Birondo <jbirondo@gmail.com>
2020-06-15 22:07:09 -0700

    Working with variables

--- basics.py
+++ basics.py
@@ -1,10 +1,18 @@
 import datetime
 # print("The date and time is", datetime.datetime.now())
 
-mynow = datetime.datetime.now()
-print(mynow)
+# mynow = datetime.datetime.now()
+# print(mynow)
 
-mynumber = 10
-mytext = "Hello"
+# mynumber = 10
+# mytext = "Hello"
 
-print(mynumber, mytext)
+# print(mynumber, mytext)
+
+x = 10
+y = "10"
+
+sum1 = x + x
+sum2 = y + y
+
+print(sum1, sum2)
