e61f9413e64505db72c852a61e15858b838e292d
Eugene Birondo <jbirondo@gmail.com>
2020-06-16 18:55:01 -0700

    More on conditionals

--- basics.py
+++ basics.py
@@ -46,10 +46,11 @@ student_grades = { "Marry": 9.1, "Sim": 8.8, "John": 7.5}
 
 
 def mean(value):
-    if type(value) == dict:
+    if isinstance(value, dict):
         the_mean = sum(value.values()) / len(value)
     else:
         the_mean = sum(value) / len(value)
+
     return the_mean
 
 print(mean([1, 4, 5]))
