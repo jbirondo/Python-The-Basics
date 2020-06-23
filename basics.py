02663098fbe007096494779bbeec3e31f7904e7f
Eugene Birondo <jbirondo@gmail.com>
2020-06-23 15:52:14 -0700

    Summary: list comprrehensions

--- basics.py
+++ basics.py
@@ -126,12 +126,37 @@
 
 # print(new_temps)
 
+# temps = [221, 234, 340, -9999, 230]
+# new_temps = [temp / 10 for temp in temps if temp != -9999]
+
+# print(new_temps)
+
+# def only_numbers(list):
+#     new_list = [ele for ele in list if isinstance(ele, int)]
+#     print(new_list)
+
 temps = [221, 234, 340, -9999, 230]
-new_temps = [temp / 10 for temp in temps if temp != -9999]
+
+new_temps = [temp / 10 if temp != -9999 else 0 for temp in temps]
 
 print(new_temps)
 
-def only_numbers(list):
-    new_list = [ele for ele in list if isinstance(ele, int)]
-    print(new_list)
+# Summary: List Comprehensions
+# In this section you learned that:
 
+# A list comprehension is an expression that creates a list by iterating over another container.
+
+# A basic list comprehension:
+
+# [i*2 for i in [1, 5, 10]]
+# Output: [2, 10, 20]
+
+# List comprehension with if condition:
+
+# [i*2 for i in [1, -2, 10] if i>0]
+# Output: [2, 20]
+
+# List comprehension with an if and else condition:
+
+# [i*2 if i>0 else 0 for i in [1, -2, 10]]
+# Output: [2, 0, 20]
