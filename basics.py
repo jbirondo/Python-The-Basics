aa22633daeb3db3bdaaafeb901d1e30f7f268f78
Eugene Birondo <jbirondo@gmail.com>
2020-06-23 15:32:20 -0700

    Simple list comprehension

--- basics.py
+++ basics.py
@@ -115,10 +115,13 @@
 #Simple List Comprehension
 
 temps = [221, 234, 340, 230]
-new_temps = []
+# new_temps = []
 
-for temp in temps:
-    new_temps.append(temp/10)
+# for temp in temps:
+#     new_temps.append(temp/10)
+
+# print(new_temps)
+
+new_temps = [temp / 10 for temp in temps]
 
 print(new_temps)
-
