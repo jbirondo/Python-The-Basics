95056977d203a03dc33e973c84edc6deaeb7fe26
Eugene Birondo <jbirondo@gmail.com>
2020-06-26 11:49:37 -0700

    Third party module example

--- basics.py
+++ basics.py
@@ -306,11 +306,12 @@ with open("files/vegetables.txt", "w") as myfile:
 
 import time
 import os
+import pandas
 
 while True:
-    if os.path.exists("files/vegetables.txt"):
-        with open("files/vegetables.txt") as file:
-            print(file.read())
+    if os.path.exists("files/temps_today.csv"):
+        data = pandas.read_csv("files/temps_today.csv")
+        print(data.mean())
     else:
         print("File does not exist")
     time.sleep(10)
