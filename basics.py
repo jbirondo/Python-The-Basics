a041cb4a8e2b50f89c481589d6bb452eac9ecf64
Eugene Birondo <jbirondo@gmail.com>
2020-06-26 11:52:34 -0700

    Summary: imported modules

--- basics.py
+++ basics.py
@@ -316,35 +316,35 @@ with open("files/vegetables.txt", "w") as myfile:
 #         print("File does not exist")
 #     time.sleep(10)
 
-Summary: Imported Modules
-In this section you learned that:
+# Summary: Imported Modules
+# In this section you learned that:
 
-Builtin objects are all objects that are written inside the Python interpreter in C language.
+# Builtin objects are all objects that are written inside the Python interpreter in C language.
 
-Builtin modules contain builtins objects.
+# Builtin modules contain builtins objects.
 
-Some builtin objects are not immediately available in the global namespace. They are parts of a builtin module. To use those objects the module needs to be imported first. E.g.:
+# Some builtin objects are not immediately available in the global namespace. They are parts of a builtin module. To use those objects the module needs to be imported first. E.g.:
 
-import time
-time.sleep(5)
-A list of all builtin modules can be printed out with:
+# import time
+# time.sleep(5)
+# A list of all builtin modules can be printed out with:
 
-import sys
-sys.builtin_module_names
-Standard libraries is a jargon that includes both builtin modules written in C and also modules written in Python.
+# import sys
+# sys.builtin_module_names
+# Standard libraries is a jargon that includes both builtin modules written in C and also modules written in Python.
 
-Standard libraries written in Python reside in the Python installation directory as .py files. You can find their directory path with sys.prefix.
+# Standard libraries written in Python reside in the Python installation directory as .py files. You can find their directory path with sys.prefix.
 
-Packages are a collection of .py modules.
+# Packages are a collection of .py modules.
 
-Third-party libraries are packages or modules written by third-party persons (not the Python core development team).
+# Third-party libraries are packages or modules written by third-party persons (not the Python core development team).
 
-Third-party libraries can be installed from the terminal/command line:
+# Third-party libraries can be installed from the terminal/command line:
 
-Windows:
+# Windows:
 
-pip install pandas or use python -m pip install pandas if that doesn't work.
+# pip install pandas or use python -m pip install pandas if that doesn't work.
 
-Mac and Linux:
+# Mac and Linux:
 
-pip3 install pandas or use python3 -m pip install pandas if that doesn't work.
+# pip3 install pandas or use python3 -m pip install pandas if that doesn't work.
