68043ce74ce14066d436afeb48e68fc483f11e49
Eugene Birondo <jbirondo@gmail.com>
2020-06-25 18:10:16 -0700

    Coding exercise:  copy n-times

--- basics.py
+++ basics.py
@@ -271,3 +271,10 @@ with open("files/vegetables.txt", "w") as myfile:
     
 # with open("bear2.txt", "a+") as second:
 #     second.write(content)
+
+# with open("data.txt", "a+") as file:
+#     file.seek(0)
+#     content = file.read()
+#     file.seek(0)
+#     file.write(content)
+#     file.write(content)
