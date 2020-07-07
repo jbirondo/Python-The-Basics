3d466466c246095e7ea7583fde7709e6027295b6
Eugene Birondo <jbirondo@gmail.com>
2020-07-06 23:12:13 -0700

    Working on Playing with digits

--- exercises.py
+++ exercises.py
@@ -21,3 +21,6 @@
 # dig_pow(92, 1) should return -1 since there is no k such as 9¹ + 2² equals 92 * k
 # dig_pow(695, 2) should return 2 since 6² + 9³ + 5⁴= 1390 = 695 * 2
 # dig_pow(46288, 3) should return 51 since 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
+
+def dig_pow(n, p):
+    return -1
