cf6f037e988a63da141685d6014d36b3d2f1727a
Eugene Birondo <jbirondo@gmail.com>
2020-07-07 20:45:37 -0700

    Triangle of consecutive odd numbers

--- exercises.py
+++ exercises.py
@@ -41,3 +41,16 @@
 # print(int_to_list(123123))
 # print(dig_pow(46288, 3))
 # print(isinstance(int_to_list(123123), str)) == True
+
+# Given the triangle of consecutive odd numbers:
+
+#              1
+#           3     5
+#        7     9    11
+#    13    15    17    19
+# 21    23    25    27    29
+# ...
+# Calculate the row sums of this triangle from the row index (starting at index 1) e.g.:
+
+# row_sum_odd_numbers(1); # 1
+# row_sum_odd_numbers(2); # 3 + 5 = 8
