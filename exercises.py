abf7f5bf031a6e35d71bf09866751305f7a30d09
Eugene Birondo <jbirondo@gmail.com>
2020-07-08 22:17:13 -0700

    Lost without a map

--- exercises.py
+++ exercises.py
@@ -93,11 +93,13 @@
 # def opposite(number):
 #     return number * -1
 
-Given an array of integers, return a new array with each value doubled.
+# Given an array of integers, return a new array with each value doubled.
 
-For example:
+# For example:
 
-[1, 2, 3] --> [2, 4, 6]
+# [1, 2, 3] --> [2, 4, 6]
 
-For the beginner, try to use the map method - it comes in very handy quite a lot so is a good one to know.
+# For the beginner, try to use the map method - it comes in very handy quite a lot so is a good one to know.
+
+
 
