6516bf575b5987023d9b1b5996185ef6126a4e4c
Eugene Birondo <jbirondo@gmail.com>
2020-07-10 12:41:33 -0700

    Array diff

--- exercises.py
+++ exercises.py
@@ -200,3 +200,18 @@
 #         num = num - new_num
 #     return " + ".join(result)
 # print(expanded_form(7000120003))
+
+# Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+# It should remove all values from list a, which are present in list b.
+
+# array_diff([1,2],[1]) == [2]
+# If a value is present in b, all of its occurrences must be removed from the other:
+
+# array_diff([1,2,2,2,3],[2]) == [1,3]
+
+def array_diff(a, b):
+    for ele in b:
+        if ele in a:
+            new_set.remove(ele)
+    return list(new_set)
