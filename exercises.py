d538dbc7894eea7c53d035a816bd21e38b36fed2
Eugene Birondo <jbirondo@gmail.com>
2020-07-09 21:54:25 -0700

    Sum of positive integers

--- exercises.py
+++ exercises.py
@@ -169,3 +169,9 @@
 #             return num
 #         num = num + 1
 #     return -1
+
+You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0.
