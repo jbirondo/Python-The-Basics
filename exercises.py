c3028a1884679aad4e94e98c7e61132eb327502c
Eugene Birondo <jbirondo@gmail.com>
2020-07-10 13:08:01 -0700

    Jaden case

--- exercises.py
+++ exercises.py
@@ -229,7 +229,7 @@
 # Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
 # Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
 
-def to_jaden_case(string):
-    array = string.split()
-    return " ".join([ele.capitalize() for ele in array])
+# def to_jaden_case(string):
+#     array = string.split()
+#     return " ".join([ele.capitalize() for ele in array])
 
