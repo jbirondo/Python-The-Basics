7ecd7ea3ec9f795706e741e2a2a0e13be814908f
Eugene Birondo <jbirondo@gmail.com>
2020-07-14 19:35:10 -0700

    Calculating with Functions

--- exercises.py
+++ exercises.py
@@ -535,18 +535,30 @@
 # def times(n):      return "*%s" % n
 # def divided_by(n): return "/%s" % n
 
-def zero(cb=None): return cb(0) if cb else 0
-def one(cb=None): return cb(1) if cb else 1
-def two(cb=None): return cb(2) if cb else 2
-def three(cb=None): return cb(3) if cb else 3
-def four(cb=None): return cb(4) if cb else 4
-def five(cb=None): return cb(5) if cb else 5
-def six(cb=None): return cb(6) if cb else 6
-def seven(cb=None): return cb(7) if cb else 7
-def eight(cb=None): return cb(8) if cb else 8
-def nine(cb=None): return cb(9) if cb else 9
+# def zero(cb=None): return cb(0) if cb else 0
+# def one(cb=None): return cb(1) if cb else 1
+# def two(cb=None): return cb(2) if cb else 2
+# def three(cb=None): return cb(3) if cb else 3
+# def four(cb=None): return cb(4) if cb else 4
+# def five(cb=None): return cb(5) if cb else 5
+# def six(cb=None): return cb(6) if cb else 6
+# def seven(cb=None): return cb(7) if cb else 7
+# def eight(cb=None): return cb(8) if cb else 8
+# def nine(cb=None): return cb(9) if cb else 9
 
-def plus(n): return lambda x: x+n
-def minus(n): return lambda x: x-n
-def times(n): return lambda x: x*n
-def divided_by(n): return lambda x: x//n
+# def plus(n): return lambda x: x+n
+# def minus(n): return lambda x: x-n
+# def times(n): return lambda x: x*n
+# def divided_by(n): return lambda x: x//n
+
+# The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+# Examples
+# "din"      =>  "((("
+# "recede"   =>  "()()()"
+# "Success"  =>  ")())())"
+# "(( @"     =>  "))((" 
+# Notes
+
+# Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
+
