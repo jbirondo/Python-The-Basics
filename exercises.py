f258b88687659242941063e499970a77c109e939
Eugene Birondo <jbirondo@gmail.com>
2020-07-14 19:12:21 -0700

    Ones and Zeros

--- exercises.py
+++ exercises.py
@@ -449,8 +449,25 @@
 # There will always be at least one number in the input string.
 # Output string must be two numbers separated by a single space, and highest number is first.
 
-def high_and_low(numbers):
-    array = [int(x) for x in numbers.split(" ")]
-    return "{} {}".format(max(array), min(array))
+# def high_and_low(numbers):
+#     array = [int(x) for x in numbers.split(" ")]
+#     return "{} {}".format(max(array), min(array))
 
-print(high_and_low("1 2 3 4 5"))  # return "5 1"
+# print(high_and_low("1 2 3 4 5"))  # return "5 1"
+
+# Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+
+# Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+
+# Examples:
+
+# Testing: [0, 0, 0, 1] ==> 1
+# Testing: [0, 0, 1, 0] ==> 2
+# Testing: [0, 1, 0, 1] ==> 5
+# Testing: [1, 0, 0, 1] ==> 9
+# Testing: [0, 0, 1, 0] ==> 2
+# Testing: [0, 1, 1, 0] ==> 6
+# Testing: [1, 1, 1, 1] ==> 15
+# Testing: [1, 0, 1, 1] ==> 11
+# However, the arrays can have varying lengths, not just limited to 4.
+
