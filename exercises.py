5606b3bf6ba42854ba9c0aefeb6d93cec1587d8e
Eugene Birondo <jbirondo@gmail.com>
2020-07-14 18:48:22 -0700

    Prime in numbers

--- exercises.py
+++ exercises.py
@@ -415,3 +415,11 @@
 #             ret += '({}{})'.format(i, '**%d' % num if num > 1 else '')
 #         if n == 1:
 #             return ret
+
+# def primeFactors(n):
+#     i, j, p = 2, 0, []
+#     while n > 1:
+#         while n % i == 0: n, j = n / i, j + 1
+#         if j > 0: p.append([i,j])
+#         i, j = i + 1, 0
+#     return ''.join('(%d' %q[0] + ('**%d' %q[1]) * (q[1] > 1) + ')' for q in p)
