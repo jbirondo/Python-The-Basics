72acd76e1fda850ff8d6298354215c28c3f01652
Eugene Birondo <jbirondo@gmail.com>
2020-07-14 18:55:02 -0700

    Highest and lowest

--- exercises.py
+++ exercises.py
@@ -441,7 +441,6 @@
 
 # Example:
 
-# high_and_low("1 2 3 4 5")  # return "5 1"
 # high_and_low("1 2 -3 4 5") # return "5 -3"
 # high_and_low("1 9 3 4 -5") # return "9 -5"
 # Notes:
@@ -449,3 +448,9 @@
 # All numbers are valid Int32, no need to validate them.
 # There will always be at least one number in the input string.
 # Output string must be two numbers separated by a single space, and highest number is first.
+
+def high_and_low(numbers):
+    array = [int(x) for x in numbers.split(" ")]
+    return "{} {}".format(max(array), min(array))
+
+print(high_and_low("1 2 3 4 5"))  # return "5 1"
