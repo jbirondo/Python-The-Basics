f2f6af9d86944d725b858f1d375a06c9faf5da80
Eugene Birondo <jbirondo@gmail.com>
2020-07-14 18:48:03 -0700

    Prime in numbers

--- exercises.py
+++ exercises.py
@@ -403,3 +403,15 @@
 #     return True
 
 # print(primeFactors(1129019283))
+
+# def primeFactors(n):
+#     ret = ''
+#     for i in xrange(2, n + 1):
+#         num = 0
+#         while(n % i == 0):
+#             num += 1
+#             n /= i
+#         if num > 0:
+#             ret += '({}{})'.format(i, '**%d' % num if num > 1 else '')
+#         if n == 1:
+#             return ret
