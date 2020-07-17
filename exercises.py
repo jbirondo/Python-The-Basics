8b10e8c02342311ee5bc74c7b8c4d14237b760b8
Eugene Birondo <jbirondo@gmail.com>
2020-07-17 16:16:41 -0700

    Extract file name

--- exercises.py
+++ exercises.py
@@ -803,3 +803,27 @@
 #         new_word = chr(int(code))+''.join(char for char in word if not char.isdigit())
 #         words.append(new_word[:1]+new_word[-1]+new_word[2:-1]+new_word[1] if len(new_word)>2 else new_word)
 #     return ' '.join(words)
+
+# You have to extract a portion of the file name as follows:
+
+# Assume it will start with date represented as long number
+# Followed by an underscore
+# You'll have then a filename with an extension
+# it will always have an extra extension at the end
+# Inputs:
+# 1231231223123131_FILE_NAME.EXTENSION.OTHEREXTENSION
+
+# 1_This_is_an_otherExample.mpg.OTHEREXTENSIONadasdassdassds34
+
+# 1231231223123131_myFile.tar.gz2
+# Outputs
+# FILE_NAME.EXTENSION
+
+# This_is_an_otherExample.mpg
+
+# myFile.tar
+# Acceptable characters for random tests:
+
+# abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-0123456789
+
+# The recommended way to solve it is using RegEx and specifically groups.
