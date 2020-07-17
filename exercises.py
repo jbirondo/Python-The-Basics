12e1ba9f275017a97061eb559d49976df3886475
Eugene Birondo <jbirondo@gmail.com>
2020-07-17 14:17:32 -0700

    Travel

--- exercises.py
+++ exercises.py
@@ -709,40 +709,40 @@
 # Note:
 # You can see a few addresses and zipcodes in the test cases.
 
-def travel(r, zipcode):
-    l = r.split(",")
-    zips = [" ".join(x.split()[-2:]) for x in l]
-    d = {}
-    for x in zips:
-        d.update({x: []})
+# def travel(r, zipcode):
+#     l = r.split(",")
+#     zips = [" ".join(x.split()[-2:]) for x in l]
+#     d = {}
+#     for x in zips:
+#         d.update({x: []})
 
-    for y in l:
-        a = " ".join(y.split()[-2:])
-        d[a].append(y)
+#     for y in l:
+#         a = " ".join(y.split()[-2:])
+#         d[a].append(y)
     
-    if zipcode not in d:
-        return zipcode + ":/"
+#     if zipcode not in d:
+#         return zipcode + ":/"
 
-    ads = [" ".join(x.split()[1:-2]) for x in d[zipcode]]
-    nums = [x.split()[0] for x in d[zipcode]]
-    return zipcode + ":" + ",".join(ads) + "/" + ",".join(nums)
+#     ads = [" ".join(x.split()[1:-2]) for x in d[zipcode]]
+#     nums = [x.split()[0] for x in d[zipcode]]
+#     return zipcode + ":" + ",".join(ads) + "/" + ",".join(nums)
 
     
     
     
 
 
-ad = ("123 Main Street St. Louisville OH 43071,432 Main Long Road St. Louisville OH 43071,786 High Street Pollocksville NY 56432,"
-"54 Holy Grail Street Niagara Town ZP 32908,3200 Main Rd. Bern AE 56210,1 Gordon St. Atlanta RE 13000,"
-"10 Pussy Cat Rd. Chicago EX 34342,10 Gordon St. Atlanta RE 13000,58 Gordon Road Atlanta RE 13000,"
-"22 Tokyo Av. Tedmondville SW 43098,674 Paris bd. Abbeville AA 45521,10 Surta Alley Goodtown GG 30654,"
-"45 Holy Grail Al. Niagara Town ZP 32908,320 Main Al. Bern AE 56210,14 Gordon Park Atlanta RE 13000,"
-"100 Pussy Cat Rd. Chicago EX 34342,2 Gordon St. Atlanta RE 13000,5 Gordon Road Atlanta RE 13000,"
-"2200 Tokyo Av. Tedmondville SW 43098,67 Paris St. Abbeville AA 45521,11 Surta Avenue Goodtown GG 30654,"
-"45 Holy Grail Al. Niagara Town ZP 32918,320 Main Al. Bern AE 56215,14 Gordon Park Atlanta RE 13200,"
-"100 Pussy Cat Rd. Chicago EX 34345,2 Gordon St. Atlanta RE 13222,5 Gordon Road Atlanta RE 13001,"
-"2200 Tokyo Av. Tedmondville SW 43198,67 Paris St. Abbeville AA 45522,11 Surta Avenue Goodville GG 30655,"
-"2222 Tokyo Av. Tedmondville SW 43198,670 Paris St. Abbeville AA 45522,114 Surta Avenue Goodville GG 30655,"
-"2 Holy Grail Street Niagara Town ZP 32908,3 Main Rd. Bern AE 56210,77 Gordon St. Atlanta RE 13000")
+# ad = ("123 Main Street St. Louisville OH 43071,432 Main Long Road St. Louisville OH 43071,786 High Street Pollocksville NY 56432,"
+# "54 Holy Grail Street Niagara Town ZP 32908,3200 Main Rd. Bern AE 56210,1 Gordon St. Atlanta RE 13000,"
+# "10 Pussy Cat Rd. Chicago EX 34342,10 Gordon St. Atlanta RE 13000,58 Gordon Road Atlanta RE 13000,"
+# "22 Tokyo Av. Tedmondville SW 43098,674 Paris bd. Abbeville AA 45521,10 Surta Alley Goodtown GG 30654,"
+# "45 Holy Grail Al. Niagara Town ZP 32908,320 Main Al. Bern AE 56210,14 Gordon Park Atlanta RE 13000,"
+# "100 Pussy Cat Rd. Chicago EX 34342,2 Gordon St. Atlanta RE 13000,5 Gordon Road Atlanta RE 13000,"
+# "2200 Tokyo Av. Tedmondville SW 43098,67 Paris St. Abbeville AA 45521,11 Surta Avenue Goodtown GG 30654,"
+# "45 Holy Grail Al. Niagara Town ZP 32918,320 Main Al. Bern AE 56215,14 Gordon Park Atlanta RE 13200,"
+# "100 Pussy Cat Rd. Chicago EX 34345,2 Gordon St. Atlanta RE 13222,5 Gordon Road Atlanta RE 13001,"
+# "2200 Tokyo Av. Tedmondville SW 43198,67 Paris St. Abbeville AA 45522,11 Surta Avenue Goodville GG 30655,"
+# "2222 Tokyo Av. Tedmondville SW 43198,670 Paris St. Abbeville AA 45522,114 Surta Avenue Goodville GG 30655,"
+# "2 Holy Grail Street Niagara Town ZP 32908,3 Main Rd. Bern AE 56210,77 Gordon St. Atlanta RE 13000")
 
-print(travel(ad, 'YY 45098'))
+# print(travel(ad, 'ZP 32908'))
