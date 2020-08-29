ee40e81f39b981f43fd4d37a50bfecafdba83c6f
Eugene Birondo <jbirondo@gmail.com>
2020-08-28 21:25:35 -0700

    Tkinter practice

--- script2.py
+++ script2.py
@@ -2,13 +2,20 @@ from tkinter import *
 
 window=Tk()
 
-b1=Button(window, text="Execute")
+def km_to_miles():
+    print(e1_value.get())
+    miles = e1_value.get() * 1.6
+    t1.insert(END, miles)
+
+b1=Button(window, text="Execute", command=km_to_miles)
 b1.grid(row=0, column=0)
 
-e1=Entry(window)
+e1_value=IntVar()
+e1=Entry(window, textvariable=e1_value)
 e1.grid(row=0, column=1)
 
-t1=Text(window, height=40, width=40)
+
+t1=Text(window, height=1, width=20)
 t1.grid(row=1, column=1)
 
 
