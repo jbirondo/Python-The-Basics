0c6022d4ab04e16a2332bb01df239e416fbe3c04
Eugene Birondo <jbirondo@gmail.com>
2020-08-30 23:43:57 -0700

    Python databases

--- script4.py
+++ script4.py
@@ -3,5 +3,6 @@ import sqlite3
 conn = sqlite3.connect("lite.db")
 cur= conn.cursor()
 cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)")
+cur.execute("INSERT INTO store VALUES ('Wine Glass', 8, 10.5)")
 conn.commit()
 conn.close()
