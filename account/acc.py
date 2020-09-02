3218a02dbb25177dd70d068dfadb495485a5c431
Eugene Birondo <jbirondo@gmail.com>
2020-09-01 17:36:31 -0700

    More OOP

--- account/acc.py
+++ account/acc.py
@@ -1,6 +1,7 @@
 class Account:
 
     def __init__(self, filepath):
+        self.filepath = filepath
         with open(filepath, "r") as file:
             self.balance=int(file.read())
 
@@ -10,5 +11,12 @@ class Account:
     def deposit(self, amount):
         self.balance = self.balance + amount
 
+    def commit(self):
+        with open(self.filepath, 'w') as file:
+            file.write(str(self.balance))
+
 account=Account("balance.txt")
 print(account.balance)
+account.withdraw(100)
+print(account.balance)
+account.commit()
