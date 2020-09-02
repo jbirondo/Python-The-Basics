44266ba4a31a7257f6f73dd38002feb455b3af5b
Eugene Birondo <jbirondo@gmail.com>
2020-09-01 17:30:34 -0700

    More OOP

--- account/acc.py
+++ account/acc.py
@@ -4,3 +4,11 @@ class Account:
         with open(filepath, "r") as file:
             self.balance=int(file.read())
 
+    def withdraw(self, amount):
+        self.balance = self.balance - amount
+
+    def deposit(self, amount):
+        self.balance = self.balance + amount
+
+account=Account("balance.txt")
+print(account.balance)
