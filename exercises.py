fd2c41542b5365c82b34717e44e8061770092940
Eugene Birondo <jbirondo@gmail.com>
2020-09-30 21:15:08 -0700

    Python CodeWars

--- exercises.py
+++ exercises.py
@@ -1549,3 +1549,10 @@
 #         except KeyError:
 #             s += char
 #     return s
+
+def encode(s, t=str.maketrans("aeiou", "12345")):
+    return s.translate(t)
+
+
+def decode(s, t=str.maketrans("12345", "aeiou")):
+    return s.translate(t)
