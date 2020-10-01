6224cf2b53e9f173cee0670a06453f943db0f442
Eugene Birondo <jbirondo@gmail.com>
2020-10-01 14:00:14 -0700

    Python CodeWars

--- exercises.py
+++ exercises.py
@@ -1642,13 +1642,13 @@
 #         ult.append(res[x] ** (x + 1))
 #     return sum(ult)
 
-def sum_dig_pow(a, b):  # range(a, b + 1) will be studied by the function
-    res = []
-    for number in range(a, b+1):
-        digits = [int(i) for i in str(number)]
-        s = 0
-        for idx, val in enumerate(digits):
-            s += val ** (idx + 1)
-        if s == number:
-            res.append(number)
-    return res
+# def sum_dig_pow(a, b):  # range(a, b + 1) will be studied by the function
+#     res = []
+#     for number in range(a, b+1):
+#         digits = [int(i) for i in str(number)]
+#         s = 0
+#         for idx, val in enumerate(digits):
+#             s += val ** (idx + 1)
+#         if s == number:
+#             res.append(number)
+#     return res
