f66586dc845317bf03e46e247275279bbebffdd2
Eugene Birondo <jbirondo@gmail.com>
2020-09-30 21:15:16 -0700

    Python CodeWars

--- exercises.py
+++ exercises.py
@@ -1550,9 +1550,9 @@
 #             s += char
 #     return s
 
-def encode(s, t=str.maketrans("aeiou", "12345")):
-    return s.translate(t)
+# def encode(s, t=str.maketrans("aeiou", "12345")):
+#     return s.translate(t)
 
 
-def decode(s, t=str.maketrans("12345", "aeiou")):
-    return s.translate(t)
+# def decode(s, t=str.maketrans("12345", "aeiou")):
+#     return s.translate(t)
