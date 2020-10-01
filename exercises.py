132dc757b4221dc951a60c5eb99d591f7297ba37
Eugene Birondo <jbirondo@gmail.com>
2020-10-01 14:00:44 -0700

    Python CodeWars

--- exercises.py
+++ exercises.py
@@ -1652,3 +1652,10 @@
 #         if s == number:
 #             res.append(number)
 #     return res
+
+def filter_func(a):
+    return sum(int(d) ** (i+1) for i, d in enumerate(str(a))) == a
+
+
+def sum_dig_pow(a, b):
+    return filter(filter_func, range(a, b+1))
