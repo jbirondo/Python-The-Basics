70ab5e2932e16f0513d4b247218cf7099d71c033
Eugene Birondo <jbirondo@gmail.com>
2020-10-01 23:09:45 -0700

    Python CodeWars

--- exercises.py
+++ exercises.py
@@ -1700,5 +1700,5 @@
 #         res.append(people[0:x] + people[x].capitalize() + people[x + 1:])
 #     return res
 
-def wave(str):
-    return [str[:i] + str[i].upper() + str[i+1:] for i in range(len(str)) if str[i].isalpha()]
+# def wave(str):
+#     return [str[:i] + str[i].upper() + str[i+1:] for i in range(len(str)) if str[i].isalpha()]
