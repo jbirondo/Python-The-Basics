b62e8f81e53908c6278d607679d46d506b2b82ab
Eugene Birondo <jbirondo@gmail.com>
2020-12-14 22:52:19 -0800

    Hackerrank compareTriplets

--- js_practice.js
+++ js_practice.js
@@ -5,3 +5,17 @@ function solveMeFirst(a, b) {
 function simpleArraySum(ar) {
     return ar.reduce((a, c) => a + c)
 }
+
+function compareTriplets(a, b) {
+    let score = [0,0]
+    for (let i = 0; i < a.length; i ++){
+        if(a[i] === b[i]){
+            continue
+        } else if (a[i] > b[i]){
+            score[0]++
+        } else {
+            score[1]++
+        }
+    }
+    return score
+}
