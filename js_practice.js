48c8739a87d211e010394979efcbd5d26be81920
Eugene Birondo <jbirondo@gmail.com>
2020-12-15 15:22:27 -0800

    Hackerrank divisibleSumPairs

--- js_practice.js
+++ js_practice.js
@@ -144,3 +144,13 @@ function birthday(s, d, m) {
     }
     return counter
 }
+
+function divisibleSumPairs(n, k, ar) {
+    let counter = 0
+    for(let i = 0; i < ar.length; i++){
+        for(let j = i + 1; j < ar.length; j++){
+            if((ar[i] + ar[j]) % k === 0) counter++
+        }
+    }
+    return counter
+}
