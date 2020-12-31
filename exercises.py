85bacd3ddcb7747ee2f7e385c2adb42a1dbcff70
Eugene Birondo <jbirondo@gmail.com>
2020-12-30 23:06:22 -0800

    Codewars first_non_consecutive

--- exercises.py
+++ exercises.py
@@ -1945,3 +1945,8 @@ def first_non_consecutive(arr):
     for x in range(0, len(arr2)):
         if arr[x] != arr2[x]:
             return arr[x]
+
+def first_non_consecutive(arr):
+    for i in range(1, len(arr)):
+    if arr[i] - arr[i-1] > 1:
+        return arr[i]
