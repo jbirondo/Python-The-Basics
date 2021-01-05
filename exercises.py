9aa674b199f74273a56a3d1399ff74cbb2b81faa
Eugene Birondo <jbirondo@gmail.com>
2021-01-04 23:33:02 -0800

    Hackerrank simpleArraySum

--- exercises.py
+++ exercises.py
@@ -1972,3 +1972,7 @@ from collections import Counter
 
 def get_strings(city):
     return ",".join(f"{char}:{'*'*count}" for char, count in Counter(city.replace(" ", "").lower()).items())
+
+def simpleArraySum(ar):
+    return sum(ar)
+
