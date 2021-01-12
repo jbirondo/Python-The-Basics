684abc5574e8b3b2760d06728dd6442e8c02889f
Eugene Birondo <jbirondo@gmail.com>
2021-01-11 23:53:47 -0800

    Runestone stack review

--- exercises.py
+++ exercises.py
@@ -2141,3 +2141,30 @@ class solution:
         name = user['data-username']
         clan, honor = (tag.text for tag in user.find("td", {"class", "is-big"}).next_siblings)
         return cls.user(name, clan, int(honor.replace(',', '')))
+    
+class Stack:
+    """Stack implementation as a list"""
+
+    def __init__(self):
+        """Create new stack"""
+        self._items = []
+
+    def is_empty(self):
+        """Check if the stack is empty"""
+        return not bool(self._items)
+
+    def push(self, item):
+        """Add an item to the stack"""
+        self._items.append(item)
+
+    def pop(self):
+        """Remove an item from the stack"""
+        return self._items.pop()
+
+    def peek(self):
+        """Get the value of the top item in the stack"""
+        return self._items[-1]
+
+    def size(self):
+        """Get the number of items in the stack"""
+        return len(self._items)
