ade351d3446a735f5079ba48dc1eac0cb7570d93
Eugene Birondo <jbirondo@gmail.com>
2021-01-13 22:31:25 -0800

    DS3 practice

--- exercises.py
+++ exercises.py
@@ -2222,3 +2222,25 @@ class Node:
     def __str__(self):
         """String"""
         return str(self._data)
+
+from pythonds3.basic import Stack
+
+
+def par_checker(symbol_string):
+    s = Stack()
+    for symbol in symbol_string:
+        if symbol == "(":
+            s.push(symbol)
+        else:
+            if s.is_empty():
+                return False
+            else:
+                s.pop()
+
+    return s.is_empty()
+
+
+print(par_checker("((()))"))  # expected True
+print(par_checker("((()()))"))  # expected True
+print(par_checker("(()"))  # expected False
+print(par_checker(")("))  # expected False
