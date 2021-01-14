cfb1dedafc2d6c8340e2cbd72bfeebf084e80dec
Eugene Birondo <jbirondo@gmail.com>
2021-01-13 22:32:59 -0800

    DS3 practice

--- exercises.py
+++ exercises.py
@@ -2327,3 +2327,32 @@ def infix_to_postfix(infix_expr):
 print(infix_to_postfix("A * B + C * D"))
 print(infix_to_postfix("( A + B ) * C - ( D - E ) * ( F + G )"))
 
+from pythonds3.basic import Stack
+
+def postfix_eval(postfix_expr):
+    operand_stack = Stack()
+    token_list = postfix_expr.split()
+
+    for token in token_list:
+        if token in "0123456789":
+            operand_stack.push(int(token))
+        else:
+            operand2 = operand_stack.pop()
+            operand1 = operand_stack.pop()
+            result = do_math(token, operand1, operand2)
+            operand_stack.push(result)
+    return operand_stack.pop()
+
+
+def do_math(op, op1, op2):
+    if op == "*":
+        return op1 * op2
+    elif op == "/":
+        return op1 / op2
+    elif op == "+":
+        return op1 + op2
+    else:
+        return op1 - op2
+
+
+print(postfix_eval("7 8 + 3 2 + /"))
