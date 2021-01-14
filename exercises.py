e1d17df4db060fdb1c3d96537ee50945bc28c8ab
Eugene Birondo <jbirondo@gmail.com>
2021-01-13 22:31:53 -0800

    DS3 practice

--- exercises.py
+++ exercises.py
@@ -2244,3 +2244,29 @@ print(par_checker("((()))"))  # expected True
 print(par_checker("((()()))"))  # expected True
 print(par_checker("(()"))  # expected False
 print(par_checker(")("))  # expected False
+
+from pythonds3.basic import Stack
+
+
+def balance_checker(symbol_string):
+    s = Stack()
+    for symbol in symbol_string:
+        if symbol in "([{":
+            s.push(symbol)
+        else:
+            if s.is_empty():
+                return False
+            else:
+                if not matches(s.pop(), symbol):
+                    return False
+
+    return s.is_empty()
+
+def matches(sym_left, sym_right):
+    all_lefts = "([{"
+    all_rights = ")]}"
+    return all_lefts.index(sym_left) == all_rights.index(sym_right)
+
+
+print(balance_checker('{({([][])}())}'))
+print(balance_checker('[{()]'))
