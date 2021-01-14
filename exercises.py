d814f5df74388c635e21fabea079c74565b5105d
Eugene Birondo <jbirondo@gmail.com>
2021-01-13 22:32:11 -0800

    DS3 practice

--- exercises.py
+++ exercises.py
@@ -2270,3 +2270,23 @@ def matches(sym_left, sym_right):
 
 print(balance_checker('{({([][])}())}'))
 print(balance_checker('[{()]'))
+
+from pythonds3.basic import Stack
+
+
+def divide_by_2(decimal_num):
+    rem_stack = Stack()
+
+    while decimal_num > 0:
+        rem = decimal_num % 2
+        rem_stack.push(rem)
+        decimal_num = decimal_num // 2
+
+    bin_string = ""
+    while not rem_stack.is_empty():
+        bin_string = bin_string + str(rem_stack.pop())
+
+    return bin_string
+
+print(divide_by_2(42))
+print(divide_by_2(31))
