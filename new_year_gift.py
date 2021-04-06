5fd8e1ffa9ff73192008c8318804982b90153aac
Eugene Birondo <jbirondo@gmail.com>
2021-04-05 21:57:18 -0700

    Practice problems

---
+++ new_year_gift.py
@@ -0,0 +1,96 @@
+class Solution:
+    def productExceptSelf(self, nums):
+        product = 1
+        zero_count = 0
+        #calculate the product of the array
+        for i in range(len(nums)):
+            if(nums[i] != 0):
+                product = product*nums[i]
+            else:
+                #keep track of 0s
+                zero_count+=1
+        #if > 1 zeros exist, the product will be 0 for all elements regardless
+        if(zero_count > 1):
+            return [0]*len(nums)
+        #if there are no zeros, the product except itself will be product//i
+        #if there is 1 zero, the product will be 0 for non-zero elments and the product for the zero element
+        return [product//i for i in nums] if zero_count == 0 else [product if i == 0 else 0 for i in nums] 
+
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        dict = {}
+        for x in range(0, len(nums)):
+            if nums[x] not in dict:
+                dict[nums[x]] = True
+            else:
+                return dict[nums[x]]
+        return False
+
+    def maxProfit(self, prices: List[int]) -> int:
+        buy = prices[0]
+        mx_profit = 0
+
+        for i in range(1,len(prices)):
+            profit = prices[i]-buy
+
+            if profit>mx_profit:
+                mx_profit = profit
+
+            if buy>prices[i]:
+                buy = prices[i]
+
+        return mx_profit
+
+    def maxSubArray(self, nums):
+        sumSoFar = max(nums)
+        currentSum = 0
+        for i in nums:
+            currentSum += i
+            if currentSum < 0:
+                currentSum = 0
+            else:
+                sumSoFar = max(sumSoFar, currentSum)
+        return sumSoFar
+
+    def findMin(self, nums: List[int]) -> int:
+        self.nums = nums
+        if len(nums) is 0:
+            return None
+        if len(nums) is 1:
+            return nums[0]
+        if nums[0] < nums[-1]:
+            return nums[0]
+        return self.binarySearch(0, len(nums)-1)
+    
+    def binarySearch(self, start, end):
+        if start > end:
+            return None
+        else:
+            mid = (start + end) // 2
+            if self.nums[mid] > self.nums[mid+1]:
+                return self.nums[mid+1]
+            if self.nums[mid] < self.nums[mid-1]:
+                return self.nums[mid]
+            else:
+                if self.nums[start] < self.nums[mid]:
+                    return self.binarySearch(mid + 1, end)
+                else:
+                    return self.binarySearch(start, mid - 1)
+                    
+    def search(self, nums: List[int], target: int) -> int:
+        low = 0
+        high = len(nums) - 1
+        while low <= high:
+            mid = (low + high)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] >= nums[low]:
+                if target < nums[mid] and target >= nums[low]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            else:
+                if target > nums[mid] and target <= nums[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+        return -1
