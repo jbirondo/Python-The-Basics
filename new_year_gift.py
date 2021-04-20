31ad27b789cff9a5dc338089a85de4d2b5d3d289
Eugene Birondo <jbirondo@gmail.com>
2021-04-19 23:18:45 -0700

    Leet Code climbing stairs

--- new_year_gift.py
+++ new_year_gift.py
@@ -94,3 +94,12 @@ class Solution:
                 else:
                     high = mid - 1
         return -1
+
+class Solution:
+    arr = {}
+    def climbStairs(self, n: int) -> int:
+        if n == 1: return 1
+        if n == 2: return 2
+        if not self.arr.get(n-1): self.arr[n-1] = self.climbStairs(n-1)
+        if not self.arr.get(n-2): self.arr[n-2] = self.climbStairs(n-2)
+        return self.arr[n-1] + self.arr[n-2]
