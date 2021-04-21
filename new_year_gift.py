38af6a5656454755439789bc1b12140a4b3cba11
Eugene Birondo <jbirondo@gmail.com>
2021-04-20 23:38:48 -0700

    Leet Code coin change

--- new_year_gift.py
+++ new_year_gift.py
@@ -245,3 +245,13 @@ class Solution:
         # if mincoins[amount] couldn't be used then no 
 		# combination of coins could make up target amount
         return mincoins[amount] if mincoins[amount] != float('inf') else -1
+
+class Solution:
+    def coinChange(self, coins, amount):
+        @lru_cache(None)
+        def dp(i):
+            if i == 0: return 0
+            if i < 0: return float("inf")
+            return min(dp(i - coin) + 1 for coin in coins)
+        
+        return dp(amount) if dp(amount) != float("inf") else -1
