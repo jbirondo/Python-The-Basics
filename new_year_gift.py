c9db8534d9a062ed2ab319cf13ce03f6710c52ca
Eugene Birondo <jbirondo@gmail.com>
2021-04-22 23:56:42 -0700

    Leet Code longest common subsequence

--- new_year_gift.py
+++ new_year_gift.py
@@ -377,3 +377,17 @@ class Solution:
         
         dp=[-1]*len(s)
         return helper(0,dp)
+
+# Dynamic Programming
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        words=set(wordDict)
+        dp=[False]*(len(s)+1)
+        dp[0]=True
+        for i in range(1,len(s)+1):
+            for j in range(i):
+                if dp[j] and s[j:i] in words:
+                    dp[i]=True
+                    break
+        return dp[len(s)]
