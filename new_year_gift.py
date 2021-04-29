715167d319b7f4eaec063685ec7abc015cb240fa
Eugene Birondo <jbirondo@gmail.com>
2021-04-28 23:45:02 -0700

    Leet Code unique paths

--- new_year_gift.py
+++ new_year_gift.py
@@ -947,7 +947,7 @@ class Solution:
         return ways
 	```
 
-    Everything related to the letters can be totally skipped here, cause at the end the problem can be summarized like:
+Everything related to the letters can be totally skipped here, cause at the end the problem can be summarized like:
 Number of ways to combine string numbers in groups of 1 or 2, where substrings that have leading zeros, are zero or bigger than 26 are not allowed.
 
 Example: 226
@@ -1002,3 +1002,51 @@ O(N) space complexity, because we build a memo that at least will be as long as 
 O(N) time, because we will visit at least N nodes for each string char. With memoization we prune the rest of the tree calls.
 
 Hope it helps !
+
+62. Unique Paths
+Medium
+
+5003
+
+244
+
+Add to List
+
+Share
+A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+How many possible unique paths are there?
+
+ 
+
+Example 1:
+
+
+Input: m = 3, n = 7
+Output: 28
+Example 2:
+
+Input: m = 3, n = 2
+Output: 3
+Explanation:
+From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+1. Right -> Down -> Down
+2. Down -> Down -> Right
+3. Down -> Right -> Down
+Example 3:
+
+Input: m = 7, n = 3
+Output: 28
+Example 4:
+
+Input: m = 3, n = 3
+Output: 6
+ 
+
+Constraints:
+
+1 <= m, n <= 100
+It's guaranteed that the answer will be less than or equal to 2 * 109.
+
