76505f2aa1eabc74ce4de6d2eb8040c7c9976812
Eugene Birondo <jbirondo@gmail.com>
2021-04-28 23:45:46 -0700

    Leet Code unique paths

--- new_year_gift.py
+++ new_year_gift.py
@@ -1063,3 +1063,16 @@ class Solution:
         memo[key] = self.uniquePaths(m-1, n, memo) + self.uniquePaths(m, n-1, memo)
         
         return memo[key]
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        table = [[0]*(n+1) for _ in range(m+1)]
+        table[1][1] = 1
+        
+        for i in range(m+1):
+            for j in range(n+1):
+                current = table[i][j]
+                if j+1 <= n: table[i][j+1] += current
+                if i+1 <= m: table[i+1][j] += current
+                    
+        return table[-1][-1]
