fad711b5068a7bc6b2a50fb0403b1129a39d0de0
Eugene Birondo <jbirondo@gmail.com>
2021-05-04 23:55:54 -0700

    Leet Code merge two sorted lists

--- new_year_gift.py
+++ new_year_gift.py
@@ -1624,3 +1624,18 @@ Constraints:
 The number of nodes in both lists is in the range [0, 50].
 -100 <= Node.val <= 100
 Both l1 and l2 are sorted in non-decreasing order.
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        cur = root = ListNode()
+    
+        while l1 and l2:
+            if l1.val <= l2.val:
+                cur.next = cur = ListNode(l1.val)
+                l1 = l1.next
+            else:
+                cur.next = cur = ListNode(l2.val)
+                l2 = l2.next
+
+        cur.next = l1 or l2
+        return root.next
