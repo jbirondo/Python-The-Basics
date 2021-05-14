dcec955ce86f5fe78a52a0422c5fa6a4a61baa3a
Eugene Birondo <jbirondo@gmail.com>
2021-05-13 23:42:41 -0700

    Leet Code number of islands

--- new_year_gift.py
+++ new_year_gift.py
@@ -2821,3 +2821,46 @@ class Solution(object):
         atlantic_set = bfs_traverse(atlantic)
         
         return list(atlantic_set.intersection(pacific_set))  # we want to get a list of coordinates that are reached from both edges
+
+200. Number of Islands
+Medium
+
+8433
+
+243
+
+Add to List
+
+Share
+Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+ 
+
+Example 1:
+
+Input: grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]
+Output: 1
+Example 2:
+
+Input: grid = [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]
+Output: 3
+ 
+
+Constraints:
+
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 300
+grid[i][j] is '0' or '1'.
