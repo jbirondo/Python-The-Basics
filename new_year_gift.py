ad41e9ca8b9367dde502fec582b96f85e730c1f7
Eugene Birondo <jbirondo@gmail.com>
2021-05-13 23:46:34 -0700

    Leet Code number of islands

--- new_year_gift.py
+++ new_year_gift.py
@@ -3053,3 +3053,24 @@ class Solution:
         
         
         return islands
+
+class Solution:
+    def dfs(self,grid,i,j): 
+        if i == len(grid) or i < 0 or j < 0 or j == len(grid[i]) or grid[i][j] == "0": 
+            return 
+        
+        grid[i][j] = "0"
+        
+        self.dfs(grid,i+1,j)
+        self.dfs(grid,i-1,j)
+        self.dfs(grid,i,j+1)
+        self.dfs(grid,i,j-1)
+        
+    def numIslands(self, grid: List[List[str]]) -> int:
+        count = 0
+        for i in range(len(grid)): 
+            for j in range(len(grid[i])): 
+                if grid[i][j] == "1": 
+                    count +=1
+                    self.dfs(grid, i, j)
+        return count 
