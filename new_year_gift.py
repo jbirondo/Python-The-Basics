2892388d74b5dfef000256462ab4941bdf9b299f
Eugene Birondo <jbirondo@gmail.com>
2021-05-18 23:41:02 -0700

    Leet Code spiral matrix

--- new_year_gift.py
+++ new_year_gift.py
@@ -3453,3 +3453,36 @@ class Solution:
         if col_0_should_clear:
             for r in range(n_rows):
                 matrix[r][0] = 0
+
+54. Spiral Matrix
+Medium
+
+3873
+
+671
+
+Add to List
+
+Share
+Given an m x n matrix, return all elements of the matrix in spiral order.
+
+ 
+
+Example 1:
+
+
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [1,2,3,6,9,8,7,4,5]
+Example 2:
+
+
+Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+ 
+
+Constraints:
+
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 10
+-100 <= matrix[i][j] <= 100
