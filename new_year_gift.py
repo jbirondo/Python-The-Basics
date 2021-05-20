8b459be8e93aaaf5bbf4e62cd1208418bd29a6f2
Eugene Birondo <jbirondo@gmail.com>
2021-05-19 23:15:41 -0700

    Leet Code rotate image

--- new_year_gift.py
+++ new_year_gift.py
@@ -3695,3 +3695,20 @@ Example 4:
 
 Input: matrix = [[1,2],[3,4]]
 Output: [[3,1],[4,2]]
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        #Transpose Matrix
+        
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i,n):
+                matrix[i][j], matrix[j][i] = matrix[j][i],matrix[i][j]
+        
+        #Reverse Matrix
+        for i in range(n):
+            for j in range(n//2):
+                matrix[i][j], matrix[i][n-1-j] = matrix[i][n-1-j], matrix[i][j]
