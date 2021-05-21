e345fa0116f2782d4e55d8a3436d86c10e30700c
Eugene Birondo <jbirondo@gmail.com>
2021-05-20 23:46:54 -0700

    Leet Code word search

--- new_year_gift.py
+++ new_year_gift.py
@@ -3768,3 +3768,47 @@ class Solution:
         for row in range(len(matrix)):
             for col in range(len(matrix)//2):
                 matrix[row][col],matrix[row][-col-1]=matrix[row][-col-1],matrix[row][col]
+
+79. Word Search
+Medium
+
+5736
+
+244
+
+Add to List
+
+Share
+Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+
+The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once.
+
+ 
+
+Example 1:
+
+
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+Output: true
+Example 2:
+
+
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "SEE"
+Output: true
+Example 3:
+
+
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
+Output: false
+ 
+
+Constraints:
+
+m == board.length
+n = board[i].length
+1 <= m, n <= 6
+1 <= word.length <= 15
+board and word consists of only lowercase and uppercase English letters.
+ 
+
+Follow up: Could you use search pruning to make your solution faster with a larger board?
