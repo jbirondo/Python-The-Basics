d7b65738741bef4227b81a65331ac9a190bd5f1f
Eugene Birondo <jbirondo@gmail.com>
2021-05-24 23:05:35 -0700

    Leet Code same tree

--- new_year_gift.py
+++ new_year_gift.py
@@ -4129,3 +4129,41 @@ def maxDepth(self, root: TreeNode) -> int:
 
     def maxDepth(self, root: TreeNode) -> int:
         return 0 if not root else 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+100. Same Tree
+Easy
+
+3310
+
+88
+
+Add to List
+
+Share
+Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+
+Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+ 
+
+Example 1:
+
+
+Input: p = [1,2,3], q = [1,2,3]
+Output: true
+Example 2:
+
+
+Input: p = [1,2], q = [1,null,2]
+Output: false
+Example 3:
+
+
+Input: p = [1,2,1], q = [1,1,2]
+Output: false
+ 
+
+Constraints:
+
+The number of nodes in both trees is in the range [0, 100].
+-104 <= Node.val <= 104
