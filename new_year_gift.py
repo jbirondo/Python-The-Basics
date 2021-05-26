c4704741da9a5917c5cdbe018f9dd58790abb9fa
Eugene Birondo <jbirondo@gmail.com>
2021-05-25 23:51:49 -0700

    Leet Code invert binary tree

--- new_year_gift.py
+++ new_year_gift.py
@@ -4239,3 +4239,38 @@ class Solution:
                 return True
         
         return twoTreeTraversal(p,q)
+
+226. Invert Binary Tree
+Easy
+
+5282
+
+78
+
+Add to List
+
+Share
+Given the root of a binary tree, invert the tree, and return its root.
+
+ 
+
+Example 1:
+
+
+Input: root = [4,2,7,1,3,6,9]
+Output: [4,7,2,9,6,3,1]
+Example 2:
+
+
+Input: root = [2,1,3]
+Output: [2,3,1]
+Example 3:
+
+Input: root = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
