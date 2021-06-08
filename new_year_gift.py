5a233873bc2b1119ab11523d3135d773a9e223ba
Eugene Birondo <jbirondo@gmail.com>
2021-06-07 23:55:49 -0700

    Leet Code binary tree lowest common ancestor

--- new_year_gift.py
+++ new_year_gift.py
@@ -5850,3 +5850,27 @@ The number of nodes in the tree is in the range [2, 105].
 All Node.val are unique.
 p != q
 p and q will exist in the BST.
+
+'''
+Time Complexity O(log(n)) where n is number of nodes in Tree.
+Process:
+Since Tree is BST, we will follow BST property which is left node<root<right node.
+For example for tree root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+p(2)<root(6)<q(8) so root 6 is answer.
+
+'''
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        node1=p if p.val<q.val else q
+        node2=q if q.val>p.val else p
+        res= self.lcautil(root,node1,node2)
+        return res
+    
+    def lcautil(self,root,node1,node2):
+        if node1.val<=root.val and root.val<=node2.val:
+            return root
+        elif root.val>node1.val and root.val>node2.val:
+            return self.lcautil(root.left,node1,node2)
+        else:
+            return self.lcautil(root.right,node1,node2)
