b6c5c51520393283a312e199d7265607c37551e6
Eugene Birondo <jbirondo@gmail.com>
2021-06-08 23:36:07 -0700

    Leet Code binary tree implement trie

--- new_year_gift.py
+++ new_year_gift.py
@@ -5909,3 +5909,47 @@ def find_lca(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode'
 		return self.find_lca(root.left, p, q)
 	elif (root.val<p.val and root.val<q.val):
 		return self.find_lca(root.right, p, q)
+
+208. Implement Trie (Prefix Tree)
+Medium
+
+4684
+
+71
+
+Add to List
+
+Share
+A trie (pronounced as "try") or prefix tree is a tree data structure used to efficiently store and retrieve keys in a dataset of strings. There are various applications of this data structure, such as autocomplete and spellchecker.
+
+Implement the Trie class:
+
+Trie() Initializes the trie object.
+void insert(String word) Inserts the string word into the trie.
+boolean search(String word) Returns true if the string word is in the trie (i.e., was inserted before), and false otherwise.
+boolean startsWith(String prefix) Returns true if there is a previously inserted string word that has the prefix prefix, and false otherwise.
+ 
+
+Example 1:
+
+Input
+["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
+[[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+Output
+[null, null, true, false, true, null, true]
+
+Explanation
+Trie trie = new Trie();
+trie.insert("apple");
+trie.search("apple");   // return True
+trie.search("app");     // return False
+trie.startsWith("app"); // return True
+trie.insert("app");
+trie.search("app");     // return True
+ 
+
+Constraints:
+
+1 <= word.length, prefix.length <= 2000
+word and prefix consist only of lowercase English letters.
+At most 3 * 104 calls in total will be made to insert, search, and startsWith.
