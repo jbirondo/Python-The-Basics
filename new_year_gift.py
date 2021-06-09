c6d5ea805dab9b3725fde0270d82e5c1336537d9
Eugene Birondo <jbirondo@gmail.com>
2021-06-08 23:56:17 -0700

    Leet Code binary tree implement trie

--- new_year_gift.py
+++ new_year_gift.py
@@ -6050,3 +6050,53 @@ class Trie:
 # obj.insert(word)
 # param_2 = obj.search(word)
 # param_3 = obj.startsWith(prefix)
+
+from collections import defaultdict
+
+class Trie:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.node = defaultdict(dict)
+        
+    def insert(self, word: str) -> None:
+        """
+        Inserts a word into the trie.
+        """
+        node = self.node 
+        for char in word:
+            if char not in node:
+                node[char] = {}
+            node = node[char]
+        node['__end__'] = True     
+        
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the trie.
+        """
+        node = self.node 
+        for char in word:
+            if char not in node: return False       
+            node = node[char]       
+        return True if '__end__' in node else False
+        
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+        node = self.node
+        for p in prefix:
+            if p not in node: return False 
+            node = node[p] 
+        return True 
+        
+        
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
