8be75ad49f95e26c35bf51fb75109b150a6ece13
Eugene Birondo <jbirondo@gmail.com>
2021-06-11 23:50:47 -0700

    Leet Code interval insert

--- new_year_gift.py
+++ new_year_gift.py
@@ -6830,3 +6830,39 @@ intervals[i].length == 2
 intervals is sorted by intervals[i][0] in ascending order.
 newInterval.length == 2
 0 <= newInterval[0] <= newInterval[1] <= 105
+
+class Solution:
+    def insert(self, intervals: List[List[int]], insert: List[int]) -> List[List[int]]:
+        result = []
+        if not len(intervals):
+            return [insert]
+        chk = False
+        if insert[0] <= intervals[0][0]:
+            result.append(insert)
+            chk = True
+        i = 0
+        while i < len(intervals):
+            n = len(result)
+            if n == 0 or (result[n-1][1]<intervals[i][0] and (chk or intervals[i][0]<=insert[0])) :
+                result.append(intervals[i])
+
+            elif not chk:
+                if result[n-1][1]<insert[0]:
+                    result.append(insert)
+                else:
+                    result[n-1][1] = max(result[n-1][1],insert[1])
+                    result[n-1][0] = min(result[n-1][0],insert[0])
+                chk = True
+                i-=1
+            else:
+                result[n-1][1] = max(result[n-1][1],intervals[i][1])
+                result[n-1][0] = min(result[n-1][0],intervals[i][0])
+            i+=1
+        if not chk:
+            n = len(result)
+            if result[n-1][1]<insert[0]:
+                result.append(insert)
+            else:
+                result[n - 1][1] = max(result[n - 1][1], insert[1])
+                result[n - 1][0] = min(result[n - 1][0], insert[0])
+        return result
