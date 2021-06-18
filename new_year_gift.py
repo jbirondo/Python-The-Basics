4a14c352cbf64ed25737673a95f7e57b83ebe9bb
Eugene Birondo <jbirondo@gmail.com>
2021-06-17 23:38:14 -0700

    Leet Code binary counting bits

--- new_year_gift.py
+++ new_year_gift.py
@@ -7306,3 +7306,10 @@ class Solution:
         for i in range(1,n+1):
             dp[i]=dp[i>>1]+i%2
         return dp
+
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        ans=[0]*(n+1)
+        for i in range(1,n+1):
+            ans[i]=(i&1)+ans[i>>1]
+        return ans
