bd5f434a2ca8632096dc0f7d2c3c2b01de1bd22e
Eugene Birondo <jbirondo@gmail.com>
2021-06-28 23:27:38 -0700

    Leet Code array max subarray

--- new_year_gift.py
+++ new_year_gift.py
@@ -7963,3 +7963,40 @@ class Solution:
             ans[i] = total//nums[i]
             
         return ans
+
+53. Maximum Subarray
+Easy
+
+12729
+
+609
+
+Add to List
+
+Share
+Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+ 
+
+Example 1:
+
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+Example 2:
+
+Input: nums = [1]
+Output: 1
+Example 3:
+
+Input: nums = [5,4,-1,7,8]
+Output: 23
+ 
+
+Constraints:
+
+1 <= nums.length <= 3 * 104
+-105 <= nums[i] <= 105
+ 
+
+Follow up: If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
