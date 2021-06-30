c85078b2390b845a458f1c4dad58c1d39955989a
Eugene Birondo <jbirondo@gmail.com>
2021-06-29 23:24:33 -0700

    Leet Code array max product subarray

--- new_year_gift.py
+++ new_year_gift.py
@@ -8139,3 +8139,15 @@ class Solution:
             ma[i] = max(v,ma[i-1]*v,mi[i-1]*v)
             ans = max(ans,mi[i],ma[i])
         return ans
+
+def maxProduct(self, nums: List[int]) -> int:        
+	ans = -math.inf
+	n = len(nums)
+	mi,ma = 1,1
+	for i in range(1,n+1):
+		v = nums[i-1]
+		new_mi = min(v,mi*v,ma*v)
+		ma = max(v,ma*v,mi*v)
+		mi = new_mi
+		ans = max(ans,mi,ma)
+	return ans
