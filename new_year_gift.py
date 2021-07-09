f7ec049d9e15387735ce9dcf4736ca9cb6fd6bef
Eugene Birondo <jbirondo@gmail.com>
2021-07-08 23:25:40 -0700

    Leet Code dynamic programming climbing stairs

--- new_year_gift.py
+++ new_year_gift.py
@@ -8734,3 +8734,14 @@ class Solution:
         sum_out += 1 # Add the last config when all is 1
             
         return int(sum_out)
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if (n == 1):
+            return 1
+        if (n == 2):
+            return 2
+        res = [1,2]
+        for i in range(2,n):
+            res.append(res[i-1] + res[i-2])
+        return res[-1]
