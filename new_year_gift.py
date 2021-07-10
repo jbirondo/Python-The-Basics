8922e634b3283af10ba3b886c0077cfdaf3e560f
Eugene Birondo <jbirondo@gmail.com>
2021-07-09 23:15:27 -0700

    Leet Code dynamic programming coin change

--- new_year_gift.py
+++ new_year_gift.py
@@ -8809,3 +8809,33 @@ class Solution(object):
                 if coin <= amt:
                     ways[amt] = min(ways[amt], 1 + ways[amt-coin])
         return ways[amount] if ways[amount] != float('inf') else -1
+
+def coinChange(coins, amount):
+    
+	if amount == 0:
+		return 0
+	from collections import deque
+	q = deque()
+	q.append(amount) # amt, depth
+	visited = set()
+	depth = 0
+	while q:
+		for i in range(len(q)):
+			amt = q.popleft()
+
+			if amt < 0: # skip prune branches that yeild -ve nodes
+				continue
+
+			elif amt == 0:
+				return depth
+
+			if amt not in visited: # skip nodes seen before - see explanantion above
+				visited.add(amt)
+
+				# move down a level
+				for c in coins: 
+					q.append(amt-c)
+
+		depth += 1
+
+	return -1
