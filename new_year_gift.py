2a924f9df15bf868156b658add24847220b18fa5
Eugene Birondo <jbirondo@gmail.com>
2021-07-12 23:14:33 -0700

    Leet Code dynamic programming increasing subsequence

--- new_year_gift.py
+++ new_year_gift.py
@@ -8952,3 +8952,13 @@ Example 3:
 
 Input: nums = [7,7,7,7,7,7,7]
 Output: 1
+
+class Solution:  # 2516 ms, faster than 64.96%
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [1] * n
+        for i in range(n):
+            for j in range(i):
+                if nums[i] > nums[j] and dp[i] < dp[j] + 1:
+                    dp[i] = dp[j] + 1
+        return max(dp)
