cb3044f98e95abb0f4301246d70c8b3f0f8a5049
Eugene Birondo <jbirondo@gmail.com>
2021-07-15 23:03:16 -0700

    Leet Code dynamic programming house robber

--- new_year_gift.py
+++ new_year_gift.py
@@ -9317,3 +9317,22 @@ Input: nums = [2,7,9,3,1]
 Output: 12
 Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
 Total amount you can rob = 2 + 9 + 1 = 12.
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums)==1:
+            return nums[0]
+        if len(nums)==0:
+            return 0
+        dp=[0]*len(nums)
+        if len(nums)==2:
+            return max(nums[0],nums[1])
+        dp[0]=nums[0]
+        dp[1]=max(nums[0],nums[1])
+        
+        
+        for i in range(2,len(nums)):
+            dp[i]=max(dp[i-2]+nums[i],dp[i-1])
+            
+        print(dp)
+        return dp[len(nums)-1]
