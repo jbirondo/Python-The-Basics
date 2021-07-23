0cb853e9cd54b9dd2a61093900ead9533daa3684
Eugene Birondo <jbirondo@gmail.com>
2021-07-22 22:54:36 -0700

    Leet Code tree max depth bin tree

--- new_year_gift.py
+++ new_year_gift.py
@@ -9863,3 +9863,17 @@ Example 4:
 
 Input: root = [0]
 Output: 1
+
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int: 
+        if not root:
+            return 0
+        queue = collections.deque([(root, 1)])        
+        while queue:
+            node, level = queue.popleft()
+            if node.left:
+                queue.append((node.left, level +1))
+            if node.right:
+                queue.append((node.right, level +1))
+        
+        return level
