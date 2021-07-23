09f6e3cc277b9aeb195b31ff362489448e2ce772
Eugene Birondo <jbirondo@gmail.com>
2021-07-22 22:55:08 -0700

    Leet Code tree max depth bin tree

--- new_year_gift.py
+++ new_year_gift.py
@@ -9883,3 +9883,11 @@ class Solution:
         if root is None:
             return 0        
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        l = self.maxDepth(root.left)
+        r = self.maxDepth(root.right)
+        return 1 + max(l, r)
