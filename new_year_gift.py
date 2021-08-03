5644c9e48b6700dd9481865184cb4a20b40ca28c
Eugene Birondo <jbirondo@gmail.com>
2021-08-02 23:18:57 -0700

    Leet Code tree preoder and valid binary search tree

--- new_year_gift.py
+++ new_year_gift.py
@@ -10661,3 +10661,17 @@ def isValidBST(self, root: TreeNode) -> bool:
             right = dfs(root.right)
             return left and right
         return dfs(root)
+
+def isValidBST(self, root: TreeNode) -> bool:
+        stack = []
+        prev = None
+        while root or stack:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            if prev and prev.val >= root.val:
+                return False
+            prev = root
+            root = root.right
+        return True
