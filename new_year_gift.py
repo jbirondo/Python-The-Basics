f0080f8fb976f62028256ae16a2aed2414046a40
Eugene Birondo <jbirondo@gmail.com>
2021-08-04 23:00:02 -0700

    Leet Code tree lowest common

--- new_year_gift.py
+++ new_year_gift.py
@@ -10850,3 +10850,14 @@ class Solution:
         if root.val > p.val and root.val > q.val: return self.lowestCommonAncestor(root.left, p, q)
         if root.val < p.val and root.val < q.val: return self.lowestCommonAncestor(root.right, p, q)
         return root
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+	while root:
+            if root.val > p.val and root.val > q.val:
+                root=root.left
+            elif root.val < p.val and root.val < q.val:
+                root=root.right
+            else:
+                break
+        return root
