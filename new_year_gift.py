67deda60e577a29078ecb91b425eb001125d8641
Eugene Birondo <jbirondo@gmail.com>
2021-08-06 22:56:35 -0700

    Leet Code tree word search

--- new_year_gift.py
+++ new_year_gift.py
@@ -11190,3 +11190,38 @@ class WordDictionary:
             if re.match(word, w): return True
         
         return False
+
+class Node:
+    def __init__(self):
+        self.children = {}
+        self.is_word = False
+
+class WordDictionary:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = Node()
+
+    def addWord(self, word: str) -> None:
+        n = self.root
+        for c in word:
+            if c not in n.children:
+                n.children[c] = Node()
+            n = n.children[c]
+        n.is_word = True
+
+    def search(self, word: str) -> bool:
+         
+        def helper(w, n):
+            if not w:
+                return n.is_word
+            if w[0] != '.':
+                if w[0] not in n.children:
+                    return False
+                return helper(w[1:], n.children[w[0]])
+            else:
+                return any([helper(w[1:], n.children[x]) for x in n.children])
+                
+        return helper(word, self.root)
