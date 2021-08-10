a41130020e1f01f3412fabecd72c95b464971764
Eugene Birondo <jbirondo@gmail.com>
2021-08-09 23:35:46 -0700

    Leet Code tree word search 2

--- new_year_gift.py
+++ new_year_gift.py
@@ -11368,3 +11368,29 @@ board[i][j] is a lowercase English letter.
 1 <= words[i].length <= 10
 words[i] consists of lowercase English letters.
 All the strings of words are unique.
+
+class Solution:
+    def __init__(self, ans=None):
+        self.ans = []
+        self.dict = Counter()
+    def Findwords(self, board, temp, vis, i, j, m, n):
+        if i < 0 or j < 0 or i >= m or j >= n or vis[i][j] or len(self.dict) == len(self.ans):
+            return None
+        vis[i][j] = True
+        temp = temp + board[i][j]
+        if self.dict[temp]:
+            self.ans.add(temp)
+        self.Findwords(board, temp, vis, i, j + 1, m, n)
+        self.Findwords(board, temp, vis, i + 1, j, m, n)
+        self.Findwords(board, temp, vis, i, j - 1, m, n)
+        self.Findwords(board, temp, vis, i - 1, j, m, n)
+        temp = temp[:-1]
+        vis[i][j] = False
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        m, n, temp = len(board), len(board[0]), ''
+        self.dict, self.ans = Counter(words), set()
+        vis = [[False for i in range(n)] for i in range(m)]
+        for i in range(m):
+            for j in range(n):
+                self.Findwords(board, temp, vis, i, j, m, n)
+        return self.ans
