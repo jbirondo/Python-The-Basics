830d8a0b8417de676c5709543b0019ca17eb427f
Eugene Birondo <jbirondo@gmail.com>
2021-08-10 23:34:19 -0700

    Leet Code heap merge k lists

--- new_year_gift.py
+++ new_year_gift.py
@@ -11567,3 +11567,40 @@ class Solution:
                     self.search(board, newx, newy, trie.children[key], res, d)
                                                       
         board[row][col] = prev
+
+23. Merge k Sorted Lists
+Hard
+
+8194
+
+377
+
+Add to List
+
+Share
+You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+
+Merge all the linked-lists into one sorted linked-list and return it.
+
+ 
+
+Example 1:
+
+Input: lists = [[1,4,5],[1,3,4],[2,6]]
+Output: [1,1,2,3,4,4,5,6]
+Explanation: The linked-lists are:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+merging them into one sorted list:
+1->1->2->3->4->4->5->6
+Example 2:
+
+Input: lists = []
+Output: []
+Example 3:
+
+Input: lists = [[]]
+Output: []
