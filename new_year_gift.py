0c5e5c352abf25dde9e14e954e7e8241e3aa8b2f
Eugene Birondo <jbirondo@gmail.com>
2021-08-13 23:32:29 -0700

    Leet Code heap find e matrix set zeroes

--- new_year_gift.py
+++ new_year_gift.py
@@ -12167,3 +12167,22 @@ Follow up:
 A straightforward solution using O(mn) space is probably a bad idea.
 A simple improvement uses O(m + n) space, but still not the best solution.
 Could you devise a constant space solution?
+
+class Solution:
+    def setZeroes(self, M):
+        m, n = len(M[0]), len(M)
+        r1 = any(M[0][j] == 0 for j in range(m))
+        c1 = any(M[i][0] == 0 for i in range(n))
+        for i in range(1, n):
+            for j in range(1, m):
+                if M[i][j] == 0: M[i][0] = M[0][j] = 0
+                
+        for i in range(1, n):
+            for j in range(1, m):
+                if M[i][0] * M[0][j] == 0: M[i][j] = 0
+                
+        if r1:
+            for i in range(m): M[0][i] = 0
+                
+        if c1:
+            for j in range(n): M[j][0] = 0
