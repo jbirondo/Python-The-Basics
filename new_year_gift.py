0b6fe110ff14522b04ce4772da85425960ddeeb8
Eugene Birondo <jbirondo@gmail.com>
2021-08-19 23:42:38 -0700

    Leet Code coding challenge practice

--- new_year_gift.py
+++ new_year_gift.py
@@ -12731,3 +12731,14 @@ class Solution:
             arr.pop()
         helper(nums,i,arr,X)
         return X
+
+from itertools import combinations
+
+class Solution:
+	def subsets(self, nums: List[int]) -> List[List[int]]:
+		lst = []
+
+		for i in range(len(nums) + 1):
+			lst += list(combinations(nums, i))
+
+		return lst
