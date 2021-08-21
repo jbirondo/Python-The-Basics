6db382dc7e22149d7409f5adfcc0267d4dc8d168
Eugene Birondo <jbirondo@gmail.com>
2021-08-20 22:56:12 -0700

    Leet Code coding challenge practice

--- new_year_gift.py
+++ new_year_gift.py
@@ -12786,3 +12786,14 @@ class Solution:
             subset.append(currentSubset+[ele])
             
         return subset
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+            dp = [[] for i in range(len(nums))]
+            dp[0].append([])
+            dp[0].append([nums[0]])
+            for i in range(1, len(nums)):  
+                for subset in dp[i-1]: 
+                    dp[i].append(subset + [nums[i]])  # with the i'th element
+                    dp[i].append(subset)  # without the i'th element
+            return dp[len(nums)-1]
