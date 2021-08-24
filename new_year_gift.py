76a7a9315f1a6cd538218bf216e02125a7818d0f
Eugene Birondo <jbirondo@gmail.com>
2021-08-23 23:43:49 -0700

    Leet Code coding challenge practice

--- new_year_gift.py
+++ new_year_gift.py
@@ -12912,3 +12912,18 @@ def permute(self, nums):
 		return res
 
 return recursive(nums)
+
+def recursive(nums, perm=[], res=[]):
+        
+            if not nums: 
+                res.append(perm) # --- no need to copy as we are not popping/backtracking. Instead we're passing a new variable each time 
+
+            for i in range(len(nums)): 
+                newNums = nums[:i] + nums[i+1:]
+                # perm.append(nums[i]) # --- instead of appending to the same variable
+                newPerm = perm + [nums[i]] # --- new copy of the data/variable
+                recursive(newNums, newPerm, res) 
+                # perm.pop()  # --- no need to backtrack
+            return res
+        
+        return recursive(nums)
