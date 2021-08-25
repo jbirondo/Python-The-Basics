eba162524978f46cd3dc9e6d449a2b3a9b3356bf
Eugene Birondo <jbirondo@gmail.com>
2021-08-24 23:49:17 -0700

    Leet Code coding challenge practice

--- new_year_gift.py
+++ new_year_gift.py
@@ -12976,4 +12976,13 @@ def subsets(self, nums):
         select(j, [], 0)
     return res
 
+class Solution(object):
+    def subsets(self, nums):
+        ret = []
+        self.dfs(nums, [], ret)
+        return ret
     
+    def dfs(self, nums, path, ret):
+        ret.append(path)
+        for i in range(len(nums)):
+            self.dfs(nums[i+1:], path+[nums[i]], ret)
