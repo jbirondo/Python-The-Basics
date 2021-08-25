54ef3129a14dddbd0bb12bdc35f137d8180cc1a2
Eugene Birondo <jbirondo@gmail.com>
2021-08-24 23:49:35 -0700

    Leet Code coding challenge practice

--- new_year_gift.py
+++ new_year_gift.py
@@ -12986,3 +12986,14 @@ class Solution(object):
         ret.append(path)
         for i in range(len(nums)):
             self.dfs(nums[i+1:], path+[nums[i]], ret)
+
+    def subsets2(self, nums):
+        res = []
+        nums.sort()
+        for i in xrange(1<<len(nums)):
+            tmp = []
+            for j in xrange(len(nums)):
+                if i & 1 << j:  # if i >> j & 1:
+                    tmp.append(nums[j])
+            res.append(tmp)
+        return res
