280048b47d4b56a05ef88420d9179cf0d842abf1
Eugene Birondo <jbirondo@gmail.com>
2021-08-25 23:36:59 -0700

    Leet Code coding challenge practice

--- new_year_gift.py
+++ new_year_gift.py
@@ -13035,3 +13035,33 @@ def dfs(self, candidates, target, index, path, res):
         if i > index and candidates[i] == candidates[i-1]:
             continue
         self.dfs(candidates, target-candidates[i], i+1, path+[candidates[i]], res)
+
+51. N-Queens
+Hard
+
+3946
+
+122
+
+Add to List
+
+Share
+The n-queens puzzle is the problem of placing n queens on an n x n chessboard such that no two queens attack each other.
+
+Given an integer n, return all distinct solutions to the n-queens puzzle. You may return the answer in any order.
+
+Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space, respectively.
+
+ 
+
+Example 1:
+
+
+Input: n = 4
+Output: [[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
+Explanation: There exist two distinct solutions to the 4-queens puzzle as shown above
+Example 2:
+
+Input: n = 1
+Output: [["Q"]]
+
