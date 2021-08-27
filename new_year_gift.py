97d41bd3c638002150591561d00a02661c1ca9b7
Eugene Birondo <jbirondo@gmail.com>
2021-08-26 23:27:53 -0700

    Leet Code coding challenge practice

--- new_year_gift.py
+++ new_year_gift.py
@@ -13230,3 +13230,37 @@ Constraints:
 queries[i].length == 2
 1 <= ai, bi <= cities
 ai != bi
+
+class UnionFind:
+    def __init__(self, n):
+        self.parent = [i for i in range(n)]
+        self.size = [1] * n
+    def find(self, x):
+        if x != self.parent[x]:
+            self.parent[x] = self.find(self.parent[x]) # Path compression
+        return self.parent[x]
+    def union(self, u, v):
+        pu = self.find(u)
+        pv = self.find(v)
+        if pu == pv: return False
+        if self.size[pu] > self.size[pv]: # Union by larger size
+            self.size[pu] += self.size[pv]
+            self.parent[pv] = pu
+        else:
+            self.size[pv] += self.size[pu]
+            self.parent[pu] = pv
+        return True
+
+class Solution(object):
+    def areConnected(self, n, threshold, queries):
+        uf = UnionFind(n+1)
+        for i in range(1, n+1):
+            for j in range(i*2, n+1, i): # step by i
+                if i > threshold:
+                    uf.union(i, j)
+        ans = []
+        for q in queries:
+            pa = uf.find(q[0])
+            pb = uf.find(q[1])
+            ans.append(pa == pb)
+        return ans
