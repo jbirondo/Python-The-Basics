24a3e7da2c1e7ecd09d42cf1269213ff5eeb9740
Eugene Birondo <jbirondo@gmail.com>
2021-08-30 23:43:23 -0700

    Leetcode review

--- new_year_gift.py
+++ new_year_gift.py
@@ -13413,3 +13413,15 @@ class Solution:
             
         # max profit must be in not-hold state
         return cur_not_hold if prices else 0
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if not prices: return 0
+        sell_prc = max(prices)
+        max_prf = 0
+        for p in prices:
+            if p < sell_prc:
+                sell_prc = p
+            max_prf = max(p - sell_prc, max_prf)
+            
+        return max_prf
