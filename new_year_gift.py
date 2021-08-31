e11539d5f484d98acf1000eb75bc18b0e4941c88
Eugene Birondo <jbirondo@gmail.com>
2021-08-30 23:42:38 -0700

    Leetcode review

--- new_year_gift.py
+++ new_year_gift.py
@@ -13392,3 +13392,24 @@ def maxProfit(self, prices: List[int]) -> int:
 		maxProfit = max(maxProfit, prices[i] - minPurchase)
 		minPurchase = min(minPurchase, prices[i])
 	return maxProfit
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        # It is impossible to have stock to sell on first day, so -infinity is set as initial value
+        cur_hold, cur_not_hold = -float('inf'), 0
+        
+        
+        for stock_price in prices:
+            
+            prev_hold, prev_not_hold = cur_hold, cur_not_hold
+            
+            # either keep in hold, or just buy today with stock price
+            cur_hold = max(prev_hold, -stock_price)
+            
+            # either keep in not holding, or just sell today with stock price
+            cur_not_hold = max(prev_not_hold, prev_hold + stock_price)
+            
+            
+        # max profit must be in not-hold state
+        return cur_not_hold if prices else 0
