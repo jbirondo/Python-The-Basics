d406fc57e8eb8e4cca3bf427125a221dbed478b3
Eugene Birondo <jbirondo@gmail.com>
2021-09-07 23:59:50 -0700

    Dynamic Programming

--- new_year_gift.py
+++ new_year_gift.py
@@ -13629,3 +13629,8 @@ def climbStairs4(self, n):
     dic = [-1 for i in xrange(n)]
     dic[0], dic[1] = 1, 2
     return self.helper(n-1, dic)
+
+def helper(self, n, dic):
+    if dic[n] < 0:
+        dic[n] = self.helper(n-1, dic)+self.helper(n-2, dic)
+    return dic[n]
