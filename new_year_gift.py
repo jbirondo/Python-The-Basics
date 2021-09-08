1cb467b4936a56556ee16c11e67dcba3ae7788b3
Eugene Birondo <jbirondo@gmail.com>
2021-09-08 00:00:02 -0700

    Dynamic Programming

--- new_year_gift.py
+++ new_year_gift.py
@@ -13634,3 +13634,11 @@ def helper(self, n, dic):
     if dic[n] < 0:
         dic[n] = self.helper(n-1, dic)+self.helper(n-2, dic)
     return dic[n]
+
+def __init__(self):
+    self.dic = {1:1, 2:2}
+    
+def climbStairs(self, n):
+    if n not in self.dic:
+        self.dic[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
+    return self.dic[n]
