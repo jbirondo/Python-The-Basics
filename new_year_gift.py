4beab5a0500df9745c85e146f13054c3ebed4a9e
Eugene Birondo <jbirondo@gmail.com>
2021-09-09 23:24:37 -0700

    Dynamic Programming

--- new_year_gift.py
+++ new_year_gift.py
@@ -13831,3 +13831,18 @@ Constraints:
  
 
 Follow up: Can you come up with an algorithm that runs in O(n log(n)) time complexity?
+
+def lengthOfLIS(self, nums):
+    tails = [0] * len(nums)
+    size = 0
+    for x in nums:
+        i, j = 0, size
+        while i != j:
+            m = (i + j) / 2
+            if tails[m] < x:
+                i = m + 1
+            else:
+                j = m
+        tails[i] = x
+        size = max(i + 1, size)
+    return size
