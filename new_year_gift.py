72c366099364c05c9f373182cd27060086a1add0
Eugene Birondo <jbirondo@gmail.com>
2021-09-09 23:26:28 -0700

    Dynamic Programming

--- new_year_gift.py
+++ new_year_gift.py
@@ -13885,3 +13885,15 @@ def lengthOfLIS(self, nums: List[int]) -> int:
 				cache[prev_max_idx][idx] = max_lis(idx + 1, prev_max_idx)
 		return cache[prev_max_idx][idx]
 	return max_lis(0, -1)
+
+def lengthOfLIS(self, nums: List[int]) -> int:
+    	if not nums:
+		return 0
+	dp = [1] * len(nums)
+	max_len = 1
+	for i in range(1, len(nums)):
+		for j in range(0, i):
+			if nums[j] < nums[i]:
+				dp[i] = max(dp[i], dp[j] + 1)
+		max_len = max(max_len, dp[i])
+	return max_len
