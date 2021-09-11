207a45d2aaa521793213848c79dd8fcbcaa9dee4
Eugene Birondo <jbirondo@gmail.com>
2021-09-10 23:27:26 -0700

    Dynamic Programming

--- new_year_gift.py
+++ new_year_gift.py
@@ -14006,3 +14006,19 @@ class Solution:
                 if n < i:                       
                         dp[i] += dp[i-n]        # include all the combinations of target i-n.
         return dp[-1]
+
+def combinationSum4(self, nums: List[int], target: int) -> int:
+    
+		dp = [0]*(target+1)
+		dp[0]=0
+
+		for num in nums:
+			if num<=target:
+				dp[num] += 1
+
+		for i in range(1, len(dp)):
+			for j in nums:
+				if i>j:
+					dp[i]+=dp[i-j]
+
+		return dp[target]
