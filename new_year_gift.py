a09dfdbad53227476199588a86171ebd78ccf025
Eugene Birondo <jbirondo@gmail.com>
2021-09-10 23:26:31 -0700

    Dynamic Programming

--- new_year_gift.py
+++ new_year_gift.py
@@ -13973,3 +13973,12 @@ class Solution:
             for num in N:
                 if num + i <= T: dp[i+num] += dp[i]
         return dp[T]
+
+class Solution:
+    def combinationSum4(self, N: List[int], T: int) -> int:
+        dp = [0] * (T + 1)
+        dp[0] = 1
+        for i in range(1, T+1):
+            for num in N:
+                if num <= i: dp[i] += dp[i-num]
+        return dp[T]
