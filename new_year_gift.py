77db4df7d7959843bad177b75f36922e8289b4fe
Eugene Birondo <jbirondo@gmail.com>
2021-09-13 23:56:15 -0700

    LeetCode practice

--- new_year_gift.py
+++ new_year_gift.py
@@ -14057,3 +14057,13 @@ Constraints:
 
 1 <= nums.length <= 100
 0 <= nums[i] <= 400
+
+class Solution:
+    # @param num, a list of integer
+    # @return an integer
+    def rob(self, num):
+        max_3_house_before, max_2_house_before, adjacent = 0, 0, 0
+        for cur in num:
+            max_3_house_before, max_2_house_before, adjacent = \
+            max_2_house_before, adjacent, max(max_3_house_before+cur, max_2_house_before+cur)
+        return max(max_2_house_before, adjacent)
