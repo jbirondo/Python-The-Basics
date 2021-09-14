f54dee829b5a9851d795f41cc3989d8e769e60ca
Eugene Birondo <jbirondo@gmail.com>
2021-09-13 23:56:58 -0700

    LeetCode practice

--- new_year_gift.py
+++ new_year_gift.py
@@ -14067,3 +14067,26 @@ class Solution:
             max_3_house_before, max_2_house_before, adjacent = \
             max_2_house_before, adjacent, max(max_3_house_before+cur, max_2_house_before+cur)
         return max(max_2_house_before, adjacent)
+
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        length = len(nums)
+        if length==0:
+            return 0
+        if length==1:
+            return nums[0]
+        if length==2:
+            return max(nums)
+			
+        dp = [0]*length # assign dp array
+        dp[0], dp[1] = nums[0], max(nums[0], nums[1])
+        
+        for i in range(2, length):
+            dp[i] = max(dp[i-2]+nums[i], dp[i-1])
+        print(dp)
+        
+        return max(dp)
