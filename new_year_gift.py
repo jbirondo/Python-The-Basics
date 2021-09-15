c734c2be89bcde781c50bd4795f15e6b473f76f3
Eugene Birondo <jbirondo@gmail.com>
2021-09-14 22:52:05 -0700

    LeetCode practice

--- new_year_gift.py
+++ new_year_gift.py
@@ -14156,3 +14156,24 @@ class Solution(object):
             return nums[0]
         else:
             return max(simple_rob(nums[1:]), simple_rob(nums[:-1]))
+
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        def simple_rob(nums, i, j):
+            rob, not_rob = 0, 0
+            for idx in range(i, j):
+                num = nums[idx]
+                rob, not_rob = not_rob + num, max(rob, not_rob)
+            return max(rob, not_rob)
+        
+        if not nums:
+            return 0
+        elif len(nums) == 1:
+            return nums[0]
+        else:
+            n = len(nums)
+            return max(simple_rob(nums, 1, n), simple_rob(nums, 0, n-1))
