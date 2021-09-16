acd39fed3e4eeac40da70dd47b1538c4d47864b5
Eugene Birondo <jbirondo@gmail.com>
2021-09-15 23:41:48 -0700

    LeetCode practice

--- new_year_gift.py
+++ new_year_gift.py
@@ -14197,3 +14197,21 @@ class Solution:
         p1 = helper(nums[:len(nums) - 1])
         p2 = helper(nums[1:])
         return max(p1, p2)
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        
+        if len(nums) <= 2:
+            return max(nums)
+        
+        dp = [0] * len(nums)
+        
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        
+        for x in range(2, len(nums)):
+            dp[x] = max(dp[x - 1], nums[x] + dp[x - 2])
+        
+        return dp[-1]
