9696bc35cb92cc794d74b35ec3b4921c21d43ff9
Eugene Birondo <jbirondo@gmail.com>
2021-09-16 23:11:39 -0700

    LeetCode practice

--- new_year_gift.py
+++ new_year_gift.py
@@ -14378,3 +14378,17 @@ class Solution(object):
                     m[neigh] = Node(neigh.val)
                 m[n].neighbors.append(m[neigh])
         return m[node]
+
+    def cloneGraph2(self, node): # BFS 
+        if not node:
+            return node
+        m = {node: Node(node.val)}
+        deque = collections.deque([node])
+        while deque:
+            n = deque.popleft()
+            for neigh in n.neighbors:
+                if neigh not in m:
+                    deque.append(neigh)
+                    m[neigh] = Node(neigh.val)
+                m[n].neighbors.append(m[neigh])
+        return m[node]
