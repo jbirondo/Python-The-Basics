8522bcb88395aa2745246739395dd4d695c51ebb
Eugene Birondo <jbirondo@gmail.com>
2021-09-16 23:11:21 -0700

    LeetCode practice

--- new_year_gift.py
+++ new_year_gift.py
@@ -14363,3 +14363,18 @@ The number of nodes in the graph is in the range [0, 100].
 Node.val is unique for each node.
 There are no repeated edges and no self-loops in the graph.
 The Graph is connected and all nodes can be visited starting from the given node.
+
+class Solution(object):
+    def cloneGraph1(self, node): #DFS iteratively
+        if not node:
+            return node
+        m = {node: Node(node.val)}
+        stack = [node]
+        while stack:
+            n = stack.pop()
+            for neigh in n.neighbors:
+                if neigh not in m:
+                    stack.append(neigh)
+                    m[neigh] = Node(neigh.val)
+                m[n].neighbors.append(m[neigh])
+        return m[node]
