07dbc0c6138ad0c892a6871c99dc4099759e78f4
Eugene Birondo <jbirondo@gmail.com>
2021-09-16 23:12:26 -0700

    LeetCode practice

--- new_year_gift.py
+++ new_year_gift.py
@@ -14406,3 +14406,20 @@ class Solution(object):
                 m[neigh] = Node(neigh.val)
                 self.dfs(neigh, m)
             m[node].neighbors.append(m[neigh])
+
+class Solution(object):
+    def __init__(self):
+        self.visited = {}
+        
+    def cloneGraph(self, node):
+        if not node:
+            return None
+        if node.label in self.visited:
+            return self.visited[node.label]
+
+        clone = UndirectedGraphNode(node.label)
+        self.visited[node.label] = clone
+            
+        for n in node.neighbors:
+            clone.neighbors.append(self.cloneGraph(n))
+        return clone
